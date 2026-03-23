77bb35e0ca8ad26b5b8a0598d80934b91d487275
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-23 14:03:13 +0700

    test server GET

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"ws/src/common"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	common.LoadEnv()
+
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(200, "Welcome to my chat server version 0.0.0.1")
+	})
+
+	port := common.GetEnv("PORT")
+	fmt.Println("Server is running at http://localhost:" + port)
+	r.Run(port)
 }
