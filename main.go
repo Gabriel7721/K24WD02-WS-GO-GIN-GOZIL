e2ae640db6ccf8786fe0e03278c626e5daae0126
Gabriel7721 <116139790+Gabriel7721@users.noreply.github.com>
2026-03-27 15:00:24 +0700

    update main

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"ws/src/chat"
 	"ws/src/common"
 	"ws/src/friend"
+	"ws/src/room"
 	"ws/src/user"
 
 	"github.com/gin-gonic/gin"
@@ -17,9 +18,12 @@ func main() {
 	db := common.MongoConnect()
 	userRepo := user.NewRepository(db)
 	friendRepo := friend.NewRepository(db)
+	roomRepo := room.NewRepository(db)
+
 	userController := user.NewController(userRepo)
 	authController := auth.NewController(userRepo)
 	friendController := friend.NewController(friendRepo)
+	roomController := room.NewController(roomRepo)
 
 	go chat.WS.Run()
 
@@ -32,6 +36,8 @@ func main() {
 	r.POST("/api/login", authController.Login)
 	r.POST("/api/friend/request", auth.JWTMiddleware(), friendController.SendRequest)
 	r.POST("/api/friend/accept", auth.JWTMiddleware(), friendController.AcceptRequest)
+	r.POST("/api/room", auth.JWTMiddleware(), roomController.Create)
+
 	r.GET("/ws", chat.ServerWS)
 
 	port := common.GetEnv("PORT")
